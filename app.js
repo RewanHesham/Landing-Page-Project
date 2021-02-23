5ad67b32e2bfa0883438fc00391ab358182c8671
RewanHesham <42061331+RewanHesham@users.noreply.github.com>
2021-02-23 22:48:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,20 +22,11 @@ let myul = document.getElementById("navbar__list");
 let list = document.querySelectorAll('li');
 const links = document.getElementsByClassName('listItem');
 const buttonTop = document.querySelector('#top');
-const currentLocation = location.href;
-
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-//Add an active state to navigation items when a section is in the viewport
-for (link of links){
-    if(link.href === currentLocation){
-        link.className = "active";
-    }
-};
 
 // Button to go to top while scrolling and reaching the bottom of the page
 buttonTop.onclick = function(){
@@ -44,11 +35,11 @@ buttonTop.onclick = function(){
 };
 
 // I use this function in the onscroll() to Remove active class from all sections 
-    function removeAllActiveClasses(){
-        sections.forEach((section) => {
-            section.classList.remove("your-active-class");
-        });
-    };
+function removeAllActiveClasses(){
+    sections.forEach((section) => {
+        section.classList.remove("your-active-class");
+    });
+};
 
 /**
  * End Helper Functions
@@ -59,7 +50,7 @@ buttonTop.onclick = function(){
 // build the nav
 // This is the function i used to build the navigation menu bar list
 // also to creat sections <a> elements to use it in the event listener function of scrollToSection()
- sections.forEach( (element)=>{
+sections.forEach( (element)=>{
     let newli = document.createElement('li');
     let newa = document.createElement('a');
     newa.className = "listItem";
@@ -68,21 +59,21 @@ buttonTop.onclick = function(){
     newli.appendChild(newa);
     myul.appendChild(newli);
     newa.href+= "#"+ navlink; //Here i add the href link of each section anchor element to navigate to it
- });
+});
 
 
 // This is the scrolltoSection function to scroll to the selected section of the navigation menu
-  function scrollToSection(event){
-        event.preventDefault();
-        const targetId = event.currentTarget.getAttribute("href");
-        document.querySelector(targetId).scrollIntoView({behavior:"smooth",block:"center"});
-    };
+function scrollToSection(event){
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href");
+    document.querySelector(targetId).scrollIntoView({behavior:"smooth",block:"center"});
+};
    
 
 //active function
 // In this function i detect the postion of the sections on view and add the active class to it 
 function onscroll(){
-    var scrollPosition = document.documentElement.scrollTop;
+    let scrollPosition = document.documentElement.scrollTop;
     sections.forEach((section) => {
 
         if (
@@ -107,10 +98,11 @@ function onscroll(){
 // This is an event listener to a mouse click from the user on the navigation menu items, which will scroll down to it's section
 for(link of links){
     link.addEventListener("click", scrollToSection);
+    link.addEventListener("click", activeItem);
 };
 
 // This is an event listener to scroll on the window to mark active section
 window.addEventListener('scroll',onscroll);
 
 // This step is for creating the navigation menu bar that i get from the ul of sections
-document.body.header.appendChild(myul);
+document.header.appendChild(myul);
